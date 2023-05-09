0b2cd2031a7c294d5e25cb16189284701b004e3a
Nasirdinov Islambek <islambeknasirdinov@gmail.com>
2023-05-09 17:00:58 +0300

    Решение 2-задачи

---
+++ Lesson#1/Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a / b == b)
+{
+   Console.WriteLine("Квадрат второго числа равен на первое число");
+   Console.WriteLine(b);
+}
+else
+{
+   Console.WriteLine("Квадрат второго числа не равен на первое число");
+}
