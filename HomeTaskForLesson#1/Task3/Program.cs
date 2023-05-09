128e4812b64ef03fd75f36fb8a7a2f62f609628c
Nasirdinov Islambek <islambeknasirdinov@gmail.com>
2023-05-09 19:37:29 +0300

    Решение ДЗ№3

---
+++ HomeTaskForLesson#1/Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a % 2;
+if (b == 0)
+{
+   Console.WriteLine("да");
+}
+else
+   Console.WriteLine("нет");
+
+
