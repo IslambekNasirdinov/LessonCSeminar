8afa6108bf01fa9ebf81aec50bc06d4a597c25f5
Nasirdinov Islambek <islambeknasirdinov@gmail.com>
2023-05-09 17:33:41 +0300

    Использование switch case

--- Lesson#1/Task3/Program.cs
+++ Lesson#1/Task3/Program.cs
@@ -1,12 +1,31 @@
 ﻿Console.WriteLine("Введите число");
 int a = Convert.ToInt32(Console.ReadLine());
-if (a == 2) Console.WriteLine("Сегодня вторник");
-else if (a == 3) Console.WriteLine("Сегодня Среда");
-else if (a == 4) Console.WriteLine("Сегодня четверк");
-else if (a == 1) Console.WriteLine("Сегодня понидельник");
-else if (a == 5) Console.WriteLine("Сегодня пятница");
-else if (a == 6) Console.WriteLine("Сегодня суббота");
-else if (a == 7) Console.WriteLine("Сегодня воскресения");
-else
-   Console.WriteLine("Нет токого дня недели!");
+
+switch (a)
+{
+   case 1:
+      Console.WriteLine("Сегодня понидельник");
+      break;
+   case 2:
+      Console.WriteLine("Сегодня вторник");
+      break;
+   case 3:
+      Console.WriteLine("Сегодня Среда");
+      break;
+   case 4:
+      Console.WriteLine("Сегодня четверк");
+      break;
+   case 5:
+      Console.WriteLine("Сегодня пятница");
+      break;
+   case 6:
+      Console.WriteLine("Сегодня суббота");
+      break;
+   case 7:
+      Console.WriteLine("Сегодня воскресения");
+      break;
+   default:
+      Console.WriteLine("Нет токого дня недели!");
+      break;
+}
 
