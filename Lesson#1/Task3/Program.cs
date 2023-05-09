4426de5638749025f4b4f1cb6c4a17f66a405bed
Nasirdinov Islambek <islambeknasirdinov@gmail.com>
2023-05-09 17:23:57 +0300

    Задача 3 число и дни недели

--- Lesson#1/Task3/Program.cs
+++ Lesson#1/Task3/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a == 2) Console.WriteLine("Сегодня вторник");
+else if (a == 3) Console.WriteLine("Сегодня Среда");
+else if (a == 4) Console.WriteLine("Сегодня четверк");
+else if (a == 1) Console.WriteLine("Сегодня понидельник");
+else if (a == 5) Console.WriteLine("Сегодня пятница");
+else if (a == 6) Console.WriteLine("Сегодня суббота");
+else if (a == 7) Console.WriteLine("Сегодня воскресения");
+else
+   Console.WriteLine("Нет токого дня недели!");
+
