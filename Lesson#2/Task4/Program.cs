463716763e89f426fbe84992d85671285f0462a3
Islambek Nasirdinov <islambeknasirdinov@gmail.com>
2023-05-11 20:31:52 +0300

    ВП задачи 4

---
+++ Lesson#2/Task4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a%b==0 || b%a==0){
+    Console.WriteLine("Кратно");
+} else{
+    Console.WriteLine($"нет остаток {a%b}");
+}
