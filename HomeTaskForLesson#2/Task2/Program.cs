2ac18ff1c62c272d6522aad0ee6eb25fa3f1de4e
Islambek Nasirdinov <islambeknasirdinov@gmail.com>
2023-05-15 17:43:22 +0300

    Выполнение ДЗ Семинара № 2

---
+++ HomeTaskForLesson#2/Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number < 100){
+    Console.WriteLine("Третьей цифры нету");
+    
+    
+}else if (number > 999){
+    number = number /10;
+    int b = number % 10;
+    Console.WriteLine(b);
+}
+else{
+    int a = number %10;
+    Console.WriteLine(a);
+}
+
+
+/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6
+*/
