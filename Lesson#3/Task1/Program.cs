9c5e101ec117a93fbd38d3aefa074b09b504a6ea
Islambek Nasirdinov <islambeknasirdinov@gmail.com>
2023-05-16 19:30:24 +0300

    SP

---
+++ Lesson#3/Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int[] points = new int[2];
+
+for (int i = 0; i < points.Length; i++)
+{
+    points[i] = Convert.ToInt32(Console.ReadLine());
+}
+if (points[0] > 0 && points[1] > 0 ){
+    Console.WriteLine("1-я четверть");
+} else if (points[0] < 0 && points[1] > 0 ){
+    Console.WriteLine("2-я четверть");
+} else if (points[0] < 0 && points[1] < 0 ){
+    Console.WriteLine("3-я четверть");
+} else if (points[0] > 0 && points[1] < 0 ){
+    Console.WriteLine("4-я четверть");
+} else {
+    Console.WriteLine("x=0, y=0");
+}
---
+++ Lesson#3/Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число");
+int quater = Convert.ToInt32(Console.ReadLine());
+
+if (quater == 1){
+    Console.WriteLine("x > 0, y > 0");
+} else if (quater == 2){
+    Console.WriteLine("x < 0, y > 0");
+} else if (quater == 3){
+    Console.WriteLine("x < 0, y < 0");
+} else if (quater == 4){
+    Console.WriteLine("x > 0, y < 0");
+} else {
+    Console.WriteLine("Некоректный номер");
+}
