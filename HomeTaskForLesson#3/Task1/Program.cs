8d2fe46ebc53da4e99c4ebf8f94faca6cc281c91
Nasirdinov Islambek <islambeknasirdinov@gmail.com>
2023-05-17 07:48:19 +0300

    Выполнение ДЗ уроку 3

---
+++ HomeTaskForLesson#3/Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите пятизначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+string a = number.ToString();
+char[] array = a.ToCharArray();
+Array.Reverse(array);
+a = new String(array);
+int b = Convert.ToInt32(a);
+
+if (number == b)
+{
+   Console.WriteLine("Да");
+}
+else
+{
+   Console.WriteLine("Нет");
+}
+
+
+
+
+/*Задача 19
+
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+14212 -> нет
+
+12821 -> да
+
+23432 -> да
+*/
---
+++ HomeTaskForLesson#3/Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите координаты точки А ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты точки А ");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты точки А ");
+int z = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты точки B ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты точки B ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты точки B ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+int squareX = ((x1 - x) * (x1 - x));
+int squareY = ((y1 - y) * (y1 - y));
+int squareZ = ((z1 - z) * (z1 - z));
+
+double Length = Math.Sqrt(squareX + squareY + squareZ);
+Console.WriteLine(Length);
+
+
+
+/*Задача 21
+
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+A (3,6,8); B (2,1,-7), -> 15.84
+
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
