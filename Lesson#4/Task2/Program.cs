98538f96131773911167d43bfa2d937823a61cff
Nasirdinov Islambek <islambeknasirdinov@gmail.com>
2023-05-18 21:48:53 +0300

    Ошибки но!

---
+++ Lesson#4/Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿
+int length(int a)
+{
+   int a = Convert.ToInt32(Console.ReadLine());
+   int length = text.length;
+   for (int i = 0; i < length; i++)
+   {
+      int ;
+
+   }
+}
+
+
+/*
+Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5
+*/
