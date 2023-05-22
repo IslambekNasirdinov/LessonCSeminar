c4a07b0f6d525671cab911a417f22da29814a9dc
Islambek Nasirdinov <islambeknasirdinov@gmail.com>
2023-05-22 18:44:05 +0300

    Task4 lesson4

--- Lesson#4/Task4/Program.cs
+++ Lesson#4/Task4/Program.cs
@@ -1,10 +1,12 @@
 ﻿
 int[] points = new int[8];
-for (int i = 0; i < points.Length; i++)
+for (int i = 0; i < 8; i++)
 {
-   points[i] = new Random().Next(0, 1);
+   points[i] = new Random().Next(0, 2);
+   Console.Write(points[i] + "," );
 }
-Console.WriteLine(points);
+
+
 /*
 Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
 [1,0,1,1,0,1,0,0]
