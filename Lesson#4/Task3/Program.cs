8b55d85bd843db51af5687797a93e4023f59383e
Islambek Nasirdinov <islambeknasirdinov@gmail.com>
2023-05-22 18:36:31 +0300

    Task 4 Lesson 4

--- Lesson#4/Task3/Program.cs
+++ Lesson#4/Task3/Program.cs
@@ -1,5 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+int Sum(int number){
+    int sum = 1;
+    for (int i=1; i<=number; i++){
+        sum *= i;
+    }
+    return sum;
+}
+int result = Sum(4);
+Console.WriteLine(result);
+
+
+
 
 
 /*
